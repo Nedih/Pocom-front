b10aea37272708ba3a22a57f5b4109e2c25659fa
Nedih <49317261+Nedih@users.noreply.github.com>
2022-10-23 19:29:25 +0300

    Added form validation and bootstrap styling

--- src/elements/AppRoutes.js
+++ src/elements/AppRoutes.js
@@ -6,23 +6,23 @@ import {
 import SignUpForm from './SignUpForm.js'
 
 export default function AppRoutes() {
-    return (
-      <Routes>
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/sign_up" element={<SignUpForm/>} />
-        <Route path="/" element={<Feed />} />
-      </Routes>
-    );
-  }
+  return (
+    <Routes>
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/sign_up" element={<SignUpForm/>} />
+      <Route path="/" element={<Feed />} />
+    </Routes>
+  );
+}
   
-  function Feed() {
-    return <h2>Feed</h2>;
-  }
-  
-  function Profile() {
-    return <h2>Profile</h2>;
-  }
-  
-  function SignUp() {
-    return <h2>Sign Up</h2>;
-  }
+function Feed() {
+  return <h2>Feed</h2>;
+}
+
+function Profile() {
+  return <h2>Profile</h2>;
+}
+
+function SignUp() {
+  return <h2>Sign Up</h2>;
+}
