e2ab3aa436f37fb462a932f8dc930aea99c76825
Nedih <49317261+Nedih@users.noreply.github.com>
2022-11-11 01:39:43 +0200

    SignUp form updated

--- src/elements/SignUpForm.js
+++ src/elements/SignUpForm.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   useNavigate,
 } from "react-router-dom";
 import './SignUpForm.css';
 import {useAuth} from '../AuthContext.js'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
 
 function SignUpForm() {
   const navigate = useNavigate();
   const { setAuth} = useAuth();
+  const [startDate, setStartDate] = useState(new Date());
+  const [phone, setPhone] = useState();
 
   const handleSubmit = event => {
     event.preventDefault();
     sessionStorage.setItem('is-authorized', true);
+    console.log(phone);
     setAuth(true);
     navigate('/feed');
   };
@@ -29,20 +36,43 @@ function SignUpForm() {
             <input type="text" class="form-control" id="validationDefault02" placeholder="Last name" value="Otto" required/>
           </div>
           <div class="col-md-4 mb-3">
-            <label for="validationDefaultUsername">Username</label>
-            <div class="input-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text" id="inputGroupPrepend2">@</span>
-              </div>
-              <input type="text" class="form-control" id="validationDefaultUsername" placeholder="Username" aria-describedby="inputGroupPrepend2" required/>
-            </div>
+            <label for="validationDefault02">Middle name</label>
+            <input type="text" class="form-control" id="validationDefault02" placeholder="Last name" value="Otto" required/>
           </div>
         </div>
-        <label for="inputPassword6">Password</label>
-        <input type="password" id="inputPassword6" class="form-control mx-sm-3" aria-describedby="passwordHelpInline" required/>
-        <small id="passwordHelpInline" class="text-muted">
-          Must be 8-20 characters long.
-        </small>
+        <div class="col-md-4 mb-3">
+          <label for="validationDefaultUsername">Email</label>
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroupPrepend2">@</span>
+            </div>
+            <input type="text" class="form-control" id="validationDefaultUsername" placeholder="Username" aria-describedby="inputGroupPrepend2" required/>
+          </div>
+        </div>
+        <label for="validationDefault05" class="form-label">Birth</label>
+          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+          <label for="validationDefault05" class="form-label">Phone</label>
+          <PhoneInput
+            placeholder="Enter phone number"
+            value={phone}
+            onChange={setPhone}/>
+        <div class="col-md-4 mb-3">
+            <label for="validationDefault02">Username</label>
+            <input type="text" class="form-control" id="validationDefault02" placeholder="Last name" value="Otto" required/>
+          </div>
+          <div class="row g-3 align-items-center">
+          <div class="col-auto">
+            <label for="inputPassword6" class="col-form-label">Password</label>
+          </div>
+          <div class="col-auto">
+            <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline"/>
+          </div>
+          <div class="col-auto">
+            <span id="passwordHelpInline" class="form-text">
+              Must be 8-20 characters long.
+            </span>
+          </div>
+        </div>
         <p></p>
         <div class="form-group">
           <div class="form-check">
