779611a648d9351d6e226651fd446314017b187d
Nedih <ihor.nedaiev@nure.ua>
2022-11-29 20:43:45 +0200

    Show comments number

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -129,12 +129,13 @@ export class FeedItem extends Component {
                                 )}
                                 <span className='p1'>{post?.reactionStats && post?.reactionStats["Dislike"]}</span>
                             </span>
-                            <span onMouseEnter={() => this.handleHover(false)} onMouseLeave={() => this.handleHover(true)}>
+                            <span hidden={post?.commentsCount > 0 ? false 
+                                : (true && reactionsHidden)} onMouseEnter={() => this.handleHover(false)} onMouseLeave={() => this.handleHover(true)}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chat-square-dots" viewBox="0 0 16 16">
                                     <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
                                     <path d="M5 6a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
                                 </svg>
-                                <span className='p1'>1991</span>
+                                <span className='p1'>{post?.commentsCount}</span>
                             </span>
                         </div>                         
                     </div>
